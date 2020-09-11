ee877a65673cc952c7e4191a82a2799cb4d9be24
Mohsen Gharivand <mohsen.irim@gmail.com>
2020-09-11 15:26:59 +0430

    change ui type of movies

--- src/common/MovieItem.js
+++ src/common/MovieItem.js
@@ -3,6 +3,19 @@ import {Text, StyleSheet, Image, View, TouchableOpacity} from 'react-native';
 import {colors} from '../assets/theme';
 import {PosterImage} from './PosterImage';
 
+function colorPick(type) {
+    switch (type) {
+        case 'movie' :
+            return colors.blue;
+        case 'series' :
+            return colors.green;
+        case 'episode' :
+            return colors.red;
+        default:
+            return colors.gold;
+    }
+}
+
 const MovieItem = ({onPress, image, year, title, Type}) => {
     return (
         <TouchableOpacity activeOpacity={0.8} onPress={onPress} style={styles.buttonStyle}>
@@ -10,7 +23,7 @@ const MovieItem = ({onPress, image, year, title, Type}) => {
             <View style={styles.containerDetail}>
                 <Text numberOfLines={1} style={styles.titleText}>{title}</Text>
                 <Text style={styles.yearText}>{year}</Text>
-                <Text style={styles.typeText}>{Type}</Text>
+                <Text style={[styles.typeText, {backgroundColor: colorPick(Type)}]}>{Type}</Text>
             </View>
         </TouchableOpacity>
     );
@@ -46,23 +59,23 @@ const styles = StyleSheet.create({
     titleText: {
         fontSize: 14,
         color: colors.textColor,
-        marginTop:5
+        marginTop: 5,
     },
     yearText: {
         fontSize: 10,
         color: colors.white,
         backgroundColor: colors.gold,
-        borderRadius:5,
-        paddingHorizontal:3,
-        marginTop:5
+        borderRadius: 5,
+        paddingHorizontal: 3,
+        marginTop: 5,
     },
     typeText: {
         fontSize: 10,
         color: colors.white,
         backgroundColor: colors.gold,
-        borderRadius:5,
-        paddingHorizontal:3,
-        marginTop:3
+        borderRadius: 5,
+        paddingHorizontal: 3,
+        marginTop: 3,
     },
 });
 
